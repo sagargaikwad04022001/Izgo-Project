27f35a5e94714599eebd756894107ad6ed14f462
pravinjosh <pravinjosh5021@gmail.com>
2023-11-10 18:06:00 +0530

    Mappings done for Vehicle and Review

--- src/main/java/com/ty/izgoproject/entity/Review.java
+++ src/main/java/com/ty/izgoproject/entity/Review.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,6 +26,7 @@ public class Review {
 	private double rating;
 	@CreationTimestamp
 	private LocalDateTime createdDateTime;
-	
+	@ManyToOne
+	private User user;
 
 }
