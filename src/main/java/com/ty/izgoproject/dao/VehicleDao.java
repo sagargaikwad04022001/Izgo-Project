d10b724157695a7b914bfc569a04645526b39395
pravinjosh <pravinjosh5021@gmail.com>
2023-11-12 09:41:41 +0530

    Added VehicleDao implementations

--- src/main/java/com/ty/izgoproject/dao/VehicleDao.java
+++ src/main/java/com/ty/izgoproject/dao/VehicleDao.java
@@ -1,5 +1,43 @@
 package com.ty.izgoproject.dao;
 
-public class VehicleDao {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ty.izgoproject.entity.Vehicle;
+import com.ty.izgoproject.repository.VehicleRepository;
+@Repository
+public class VehicleDao {
+	@Autowired
+	private VehicleRepository vehicleRepository;
+	public Vehicle saveVehicle(Vehicle vehicle) {
+		return vehicleRepository.save(vehicle);
+	}
+	public Vehicle updateVehicle(Vehicle vehicle) {
+		return vehicleRepository.save(vehicle);
+	}
+	public boolean deleteVehicle(int id) {
+		Optional<Vehicle> optional=vehicleRepository.findById(id);
+		if (optional.isPresent()) {
+			Vehicle vehicle=optional.get();
+			vehicleRepository.delete(vehicle);
+			return true;
+		}
+		return false;
+	}
+	public Vehicle findVehicleById(int id) {
+		Optional<Vehicle> optional=vehicleRepository.findById(id);
+		if (optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+	}
+	public Vehicle findVehicleByVehicleNo(String vehicleNo) {
+		Vehicle vehicle =vehicleRepository.findByVehicleNo(vehicleNo);
+		if (vehicle!=null) {
+			return vehicle;
+		}
+		return null;
+	}
 }
