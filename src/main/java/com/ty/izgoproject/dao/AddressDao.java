a6942514e19a88d5a4bfe029bf5211db62beb604
pravinjosh <pravinjosh5021@gmail.com>
2023-11-13 12:08:44 +0530

    AddressDao,AddressService,VehicleDao,VehicleService implementation has been Completed

--- src/main/java/com/ty/izgoproject/dao/AddressDao.java
+++ src/main/java/com/ty/izgoproject/dao/AddressDao.java
@@ -1,5 +1,20 @@
 package com.ty.izgoproject.dao;
 
-public class AddressDao {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ty.izgoproject.entity.Address;
+import com.ty.izgoproject.repository.AddressRepository;
+
+@Repository
+public class AddressDao {
+	@Autowired
+	private AddressRepository addressRepository;
+
+	public Address updateAddress(Address address) {
+		return addressRepository.save(address);
+	}
+	
 }
