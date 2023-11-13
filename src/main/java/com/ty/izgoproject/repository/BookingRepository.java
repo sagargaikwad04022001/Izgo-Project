c454b5dfa03bf434e618d5c34d53c34770e30253
Sagar  Gaikwad <sagar.gaikwad.0018@gmail.com>
2023-11-13 09:35:03 +0530

    Booking Dao and BookingRepository implementation added

--- src/main/java/com/ty/izgoproject/repository/BookingRepository.java
+++ src/main/java/com/ty/izgoproject/repository/BookingRepository.java
@@ -1,9 +1,13 @@
 package com.ty.izgoproject.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ty.izgoproject.entity.Booking;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
+	List<Booking> findBookingByUserId(int id);
 
 }
